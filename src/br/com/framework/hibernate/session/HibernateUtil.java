ee103563bdde07f6a7206db4a185f5a8c69f8e2b
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-08-12 18:44:28 -0300

    criando session factory

---
+++ src/br/com/framework/hibernate/session/HibernateUtil.java
@@ -0,0 +1,33 @@
+package br.com.framework.hibernate.session;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.Serializable;
+
+/**
+ * Responsavel por estabelecer a conexão com hibernate
+ */
+public class HibernateUtil implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+    /**
+     * Responsavel por ler o arquivo de configuracao hibernate.cfg.xml
+     * @return sessioFactory
+     */
+    private static SessionFactory buildSessionFactory() {
+
+        try {
+            if (sessionFactory == null) {
+                sessionFactory = new Configuration().configure().buildSessionFactory();
+            }
+
+            return sessionFactory;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError("Erro ao criar conexão Session Factory");
+        }
+    }
+}
