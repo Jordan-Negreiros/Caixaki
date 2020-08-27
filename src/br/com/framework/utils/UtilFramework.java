9645cfd060b17749954c8f617c994792ae7f492d
Jordan <jordannegreirossantos@gmail.com>
2020-08-26 23:14:25 -0300

    criando UtilFramework

---
+++ src/br/com/framework/utils/UtilFramework.java
@@ -0,0 +1,17 @@
+package br.com.framework.utils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Responsável por carregar qual usuário está fazendo a operação
+ */
+@Component
+public class UtilFramework {
+    private static final long serialVersionUID = 1L;
+
+    private static final ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+
+    public synchronized static ThreadLocal<Long> getThreadLocal() {
+        return threadLocal;
+    }
+}
